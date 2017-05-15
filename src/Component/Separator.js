954e71de010a69039b6c683fd7fad7cbca94eefc
Fadi Khadra <fdkhadra@gmail.com>
2017-05-15 22:53:35 +0200

    Separator done

--- src/Component/Separator.js
+++ src/Component/Separator.js
@@ -1,5 +1,6 @@
-
 import React from 'react';
 import cssClasses from './../cssClasses';
 
-export default () => <div className={cssClasses.SEPARATOR}></div>;
+const Separator = () => <div className={cssClasses.SEPARATOR} />;
+
+export default Separator;
--- src/__tests__/Separator.js
+++ src/__tests__/Separator.js
@@ -1,13 +1,15 @@
 /* eslint-env jest */
 import React from 'react';
+import { shallow } from 'enzyme';
 import Separator from './../Component/Separator';
 import cssClasses from './../cssClasses';
-import { shallow } from 'enzyme';
 
 describe('Separator', () => {
   it('Render without crash', () => {
     const component = shallow(<Separator />);
-    expect(component.contains(<div className={cssClasses.SEPARATOR}/>))
-      .toBe(true);
+    expect(
+      component
+      .contains(<div className={cssClasses.SEPARATOR} />)
+    ).toBe(true);
   });
 });
