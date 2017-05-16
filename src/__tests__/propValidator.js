85c482c02eb73805e48d09d77664a95c12f5c27a
Fadi Khadra <fdkhadra@gmail.com>
2017-05-16 23:20:40 +0200

    Removed conditional prop-types. Too much overhead with the new prop-types package

--- src/__tests__/propValidator.js
+++ src/__tests__/propValidator.js
@@ -1,34 +1,23 @@
 /* eslint-env jest */
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import { childrenOfType, withConditionnalRequire } from './../util/propValidator';
+import {
+  childrenOfType,
+} from './../util/propValidator';
 import Item from './../Component/Item';
 
 describe('Custom PropTypes', () => {
-    describe('childrenOfType', () => {
-        it('Should return an Error if the given children type is not correct', () => {
-            const rightProps = { children: <Item />};
-            const wrongProps = { children: <div>plop</div> };
-            
-            expect(childrenOfType(Item)(wrongProps, 'children', 'TestComponent'))
-            .toBeInstanceOf(Error)
+  describe('childrenOfType', () => {
+    it('Should return an Error if the given children type is not correct',
+      () => {
+        const rightProps = { children: <Item /> };
+        const wrongProps = { children: <div>plop</div> };
 
-            expect(childrenOfType(Item)(rightProps, 'children', 'TestComponent'))
-            .toBeNull(); 
-        });
+        expect(childrenOfType(Item)(wrongProps, 'children', 'TestComponent'))
+          .toBeInstanceOf(Error);
 
-        it('Can conditionnaly require prop', () => {
-            const customString = withConditionnalRequire(PropTypes.string);
-
-            console.log(PropTypes.string);
-            const test = customString.isRequiredIf(
-                    (props, propName, componentName) => {
-                        const prop = props['children'];
-                        return typeof prop === 'undefined';
-                    }
-                    )({label: 'plop'}, 'label', 'TestComponent');
-
-        });
-    });
+        expect(childrenOfType(Item)(rightProps, 'children', 'TestComponent'))
+          .toBeNull();
+      });
+  });
 });
--- src/util/propValidator.js
+++ src/util/propValidator.js
@@ -1,5 +1,4 @@
-import { isValidElement, Children } from 'react';
-import {checkPropTypes} from 'prop-types';
+import { Children } from 'react';
 
 function withRequired(fn) {
   fn.isRequired = function (props, propName, componentName) {
@@ -15,39 +14,16 @@ function withRequired(fn) {
   return fn;
 }
 
-export function withConditionnalRequire(fn) {
-    fn.isRequiredIf = function (cb) {
-        if (Object.prototype.toString.call(cb).slice(8, -1) === 'Function') {
-                return function (props, propName, componentName) {
-                    console.log(cb(props, propName, componentName));
-                if (cb(props, propName, componentName)) {
-                   
-                   fn.isRequired(props, propName, componentName);
-                   // const prop = props[propName];
-
-                   // if (typeof prop === 'undefined') {
-                   //     return new Error(`The prop ${propName} is marked as required in 
-                     //   ${componentName}, but its value is undefined.`);
-                    //}
-                }
-
-             //   fn(props, propName, componentName);
-            };
-        }
-    }
-    return fn;
-}
-
-export const childrenOfType = type => withConditionnalRequire(
-    withRequired(
-    (props, propName, componentName) => {
-        const prop = props[propName];
-        let check = null;
-        Children.forEach(prop, child => {
-            if (child.type !== type) {
-                 check = new Error(`${componentName} expect children to be of type ${type}`);
-            }
-        });
-        return check;
-    }
-));
+export const childrenOfType = type => withRequired(
+  (props, propName, componentName) => {
+    const prop = props[propName];
+    let check = null;
+    Children.forEach(prop, child => {
+      if (child.type !== type) {
+        check = new Error(
+          `${componentName} expect children to be of type ${type}`);
+      }
+    });
+    return check;
+  }
+);
