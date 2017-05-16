7cefd70b86cde538ffd4bcb0cf1576de4e9e9d4b
Fadi Khadra <Fadi.Khadra@bisnode.com>
2017-05-16 10:25:03 +0200

    Added one more test for separator

--- src/__tests__/Separator.js
+++ src/__tests__/Separator.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import Separator from './../Component/Separator';
 import cssClasses from './../cssClasses';
 
@@ -11,5 +12,10 @@ describe('Separator', () => {
       component
       .contains(<div className={cssClasses.SEPARATOR} />)
     ).toBe(true);
+  });
+
+  it('Should not accept additional props', () => {
+    const component = shallow(<Separator foo="bar" />);
+    expect(typeof component.prop('foo') === 'undefined').toBeTruthy();
   });
 });
