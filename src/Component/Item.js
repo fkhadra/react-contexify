4bb1c7f6fca9d4b380c5f89439fff91e2615309e
Fadi Khadra <fdkhadra@gmail.com>
2017-05-17 23:51:02 +0200

    Refactored Item and test written

--- src/Component/Item.js
+++ src/Component/Item.js
@@ -1,50 +1,45 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import cssClasses from './../cssClasses';
-import cx from 'classnames';
 
 class Item extends PureComponent {
   static propTypes = {
-    label: PropTypes.node.isRequired,
-    icon: PropTypes.string,
-    leftIcon: PropTypes.string,
-    rightIcon: PropTypes.string,
+    children: PropTypes.node.isRequired,
+    target: PropTypes.object,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
     disabled: PropTypes.bool,
     onClick: PropTypes.func,
-    data: PropTypes.any,
-    target: PropTypes.any
+    data: PropTypes.any
   };
 
   static defaultProps = {
-    icon: '',
     leftIcon: '',
     rightIcon: '',
     disabled: false,
-    onClick: () => {}
+    onClick: () => {},
+    data: {}
   };
 
-  handleClick = () => this.props.onClick(this, this.props.target);
+  handleClick = () => this.props.onClick(this.props.target, this.props.data);
 
   buildItem() {
     return (
       <div className={cssClasses.ITEM_DATA}>
-        {this.hasIcon()}
-        {this.props.label}
+        {this.props.leftIcon}
+        {this.props.children}
+        {this.props.rightIcon}
       </div>
     );
-  }
-
-  hasIcon() {
-    return this.props.icon.length > 0
-            ? <span className={`${cssClasses.ITEM_ICON} ${this.props.icon}`} />
-            : '';
   }
 
   render() {
     const className = cx(cssClasses.ITEM, {
       [`${cssClasses.ITEM_DISABLED}`]: this.props.disabled
     });
+
     return (
       <div className={className} onClick={this.handleClick}>
         {this.buildItem()}
--- src/__tests__/Item.js
+++ src/__tests__/Item.js
@@ -1,11 +1,52 @@
 /* eslint-env jest */
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import Item from './../Component/Item';
 
 describe('Menu Item', () => {
-  it('Should ', () => {
-      const component = shallow(<Item />);
-      
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<Item>foo</Item>);
+  });
+
+  it('Should render without crashing', () => {
+    expect(component.html()).toContain('foo');
+  });
+
+  it('Should handle click', () => {
+    const click = jest.fn();
+    component.setProps({ onClick: click });
+
+    expect(click).not.toHaveBeenCalled();
+    component.simulate('click');
+    expect(click).toHaveBeenCalled();
+  });
+
+  it(
+    'Should pass the target and the data props to the provided callback when clicked',
+    () => {
+      const click = (target, data) => {
+        expect(target.foo).toBe('bar');
+        expect(data).toBe('data');
+      };
+      component.setProps({
+        onClick: click,
+        disabled: true,
+        target: { foo: 'bar' },
+        data: 'data'
+      });
+      component.simulate('click');
+
+    });
+
+  it('Can handle click when disabled', () => {
+    const click = jest.fn();
+    component.setProps({ onClick: click, disabled: true });
+
+    expect(click).not.toHaveBeenCalled();
+    component.simulate('click');
+    expect(click).toHaveBeenCalled();
   });
 });
