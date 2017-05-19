7127b766d4bde992ed3a8148ef9005f99c11eb88
Fadi Khadra <fdkhadra@gmail.com>
2017-05-19 12:28:58 +0200

    Rework style and animation

--- src/cssClasses.js
+++ src/cssClasses.js
@@ -2,10 +2,10 @@
 * css classes mapping
 * */
 export default {
-  MENU: 'react-contexify-menu',
-  SEPARATOR: 'react-contexify-menu__separator',
-  ITEM: 'react-contexify-menu__item',
-  ITEM_DISABLED: 'react-contexify-menu__item--disabled',
-  ITEM_DATA: 'react-contexify-menu__item__data',
-  ITEM_ICON: 'react-contexify-menu__item__icon'
+  MENU: 'react-contexify',
+  SEPARATOR: 'react-contexify__separator',
+  ITEM: 'react-contexify__item',
+  ITEM_DISABLED: 'react-contexify__item--disabled',
+  ITEM_DATA: 'react-contexify__item__data',
+  ITEM_ICON: 'react-contexify__item__icon'
 };
