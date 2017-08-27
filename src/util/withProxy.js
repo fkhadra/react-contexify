5619c9d9e0fb2586f9cbf6bf66110f7154f74787
Fadi Khadra <fdkhadra@gmail.com>
2017-08-27 22:31:57 +0200

    Created HOC to proxy any component

---
+++ src/util/withProxy.js
@@ -0,0 +1,45 @@
+/**
+ * TODO : Abstract the component
+ */
+import React, { PureComponent } from "react";
+import { render } from "react-dom";
+
+import ProxyContainer from './../Component/ProxyContainer';
+import eventManager from './../util/eventManager';
+
+const cache = [];
+
+export default function ({ component, containerId }) {
+  return class Proxy extends PureComponent {
+    componentWillMount() {
+      if (cache.indexOf(containerId) === -1) {
+        const body = document.querySelector("body");
+        const container = document.createElement("div");
+        container.id = containerId;
+        body.appendChild(container);
+        render(<ProxyContainer />, document.getElementById(containerId));
+        cache.push(containerId);
+      }
+    }
+
+    componentDidMount() {
+      this.attachToProxy(this.props);
+    }
+
+    // Maybe I need to shallow compare props
+    componentWillReceiveProps(nextProps) {
+      this.attachToProxy(nextProps);
+    }
+
+    attachToProxy(props) {
+      const C = component;
+      eventManager.emit('PROXY_RENDER', {
+        [props.id]: <C {...props} key={props.id} />
+      });
+    }
+
+    render() {
+      return null;
+    }
+  };
+}
