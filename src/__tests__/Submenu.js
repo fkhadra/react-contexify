56495c7d38539897de68499a8c50cb05be5c0c44
Fadi Khadra <fdkhadra@gmail.com>
2018-02-11 15:17:14 +0100

    Update tests

---
+++ src/__tests__/Submenu.js
@@ -0,0 +1,33 @@
+/* eslint-env jest */
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import Submenu from './../components/Submenu';
+import styles from './../components/styles';
+
+const Children = () => <div>foo</div>;
+describe('Submenu', () => {
+  it('Should render without crash 💥', () => {
+    const component = mount(<Submenu title="bar"><Children /></Submenu>);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('Should not go offscreen', () => {
+    global.innerWidth = 100;
+    global.innerHeight = 100;
+    Element.prototype.getBoundingClientRect = () => ({ right: 200, bottom: 200 });
+    const component = mount(<Submenu title="bar"><Children /></Submenu>);
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('Can be disabled with true or a function', () => {
+    let component  = mount(<Submenu title="bar" disabled><Children /></Submenu>);
+    expect(component.html()).toMatch(styles.itemDisabled);
+
+    component.unmount();
+    component  = mount(<Submenu title="bar" disabled={() => true}><Children /></Submenu>);
+    expect(component.html()).toMatch(styles.itemDisabled);
+  });
+});
