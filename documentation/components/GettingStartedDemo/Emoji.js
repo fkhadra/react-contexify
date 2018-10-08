e16ff0c62bbd5d0428abe8ef28b95ddc19847f15
Fadi Khadra <fdkhadra@gmail.com>
2018-10-08 14:39:22 +0200

    Remove docz

--- documentation/components/GettingStartedDemo/Emoji.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-export default styled.span.attrs({
-  role: "img",
-  'aria-label': "action"
-})`
-margin-right: 8px;
-`;
--- documentation/components/GettingStartedDemo/Select.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({ name, value, data, onChange }) => (
-  <select name={name} id={name} value={value} onChange={onChange}>
-    {data.map(item => (
-      <option key={item} value={item}>{item}</option>
-    ))}
-  </select>
-);
