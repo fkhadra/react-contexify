cab3eeeab684ea2d05f02b80e26b535c7aaf0828
Fadi Khadra <fdkhadra@gmail.com>
2018-10-09 21:46:00 +0200

    Rename className for consistencies

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -8,9 +8,21 @@ export const styles = {
   separator: 'react-contexify__separator',
   item: 'react-contexify__item',
   itemDisabled: 'react-contexify__item--disabled',
-  itemContent: 'react-contexify__item__data',
+  itemContent: 'react-contexify__item__content',
   itemIcon: 'react-contexify__item__icon',
   theme: 'react-contexify__theme--',
   animationWillEnter: 'react-contexify__will-enter--',
   animationWillLeave: 'react-contexify__will-leave--'
+};
+
+export const theme = {
+  light: 'light',
+  dark: 'dark'
+};
+
+export const animation = {
+  fade: 'fade',
+  flip: 'flip',
+  pop: 'pop',
+  zoom: 'zoom'
 };
