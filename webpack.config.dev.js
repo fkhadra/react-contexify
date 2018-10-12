2b94c1b66270127be7d9bd85186088007deb3f19
Fadi Khadra <fdkhadra@gmail.com>
2018-10-12 14:22:28 +0200

    Add HMR for playground

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -35,7 +35,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'playground/index.html',
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   devServer: {
     host: 'localhost',
