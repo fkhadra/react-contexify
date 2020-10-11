bd5a5c657e79d6aa448cc1659af7380f7b986702
Fadi <fdkhadra@gmail.com>
2020-10-11 18:26:55 +0200

    add portal doc

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -4,6 +4,14 @@ import { createPortal } from 'react-dom';
 import { isFn } from '../utils';
 
 export interface PortalProps {
+  /**
+   * HTML node where to mount the context menu.
+   *
+   * In SSR mode, prefer the callback form to be sure that document is only
+   * accessed on the client. `e.g. () => document.querySelector('#element')`
+   *
+   * `default: document.body`
+   */
   mountNode?: Element | (() => Element);
 }
 
