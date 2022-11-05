49e9a1303c5b2ee0398c1d05848fb392cbde34d5
Fadi Khadra <fdkhadra@gmail.com>
2022-11-05 10:37:10 +0100

    fix dom attributes

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -8,9 +8,9 @@ export const Arrow = () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <polyline points="9 18 15 12 9 6" />
   </svg>
